95f5a4d3a756a1cf77a9c4b581d0bc883be1d78d
Gyeyoung, Jung <gye0203@gmail.com>
2024-04-23 16:25:51 +0900

    added FCM

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,30 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+exports.sendScheduledNotification = functions.pubsub.schedule("0 20 * * *")
+    .timeZone("Asia/Seoul") // 시간대 설정
+    .onRun((context) => {
+      const payload = {
+        notification: {
+          title: "Daily Reminder",
+          body: "This is your daily notification at 8 PM!",
+        },
+        topic: "daily-notifications",
+      };
+
+      return admin.messaging().send(payload)
+          .then((response) => {
+            console.log("Successfully sent message:", response);
+          })
+          .catch((error) => {
+            console.log("Error sending message:", error);
+          });
+    });
+
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
