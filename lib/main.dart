9b249e3f84942ddbd08daad4a3f0edacabbdf3b4
Gyeyoung, Jung <gye0203@gmail.com>
2024-04-23 16:25:06 +0900

    added FCM

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,59 @@
 import 'package:emo_diary_project/screens/profile_setting_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'screens/loading_screen.dart';
 
-void main() {
+Future<void> _firebaseMessagingBackgroundHanlder(RemoteMessage message) async {
+  print("Background message handling...${message.notification!.body}");
+}
+
+late AndroidNotificationChannel channel;
+late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
+// Future<void> _initNotification() async {
+//   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHanlder);
+
+//   await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+//       alert: true, badge: true, sound: true);
+// }
+
+Future<void> _initFirebaseMessaging() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: 'k', appId: 'k', messagingSenderId: 'k', projectId: 'k'));
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHanlder);
+  channel = const AndroidNotificationChannel(
+      'diary_channel', 'Test Notification',
+      description: 'test', importance: Importance.high);
+
+  var initSettingsAndorid =
+      const AndroidInitializationSettings('@mipmap/ic_launcher');
+
+  flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  var initSettings = InitializationSettings(android: initSettingsAndorid);
+
+  await flutterLocalNotificationsPlugin.initialize(initSettings);
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+      alert: true, badge: true, sound: true);
+
+  print(await FirebaseMessaging.instance.getToken());
+}
+
+void main() async {
+  _initFirebaseMessaging();
   runApp(const MyApp());
 }
 
@@ -29,9 +76,31 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
+  void initFCM() {
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      var androidNotiDetails = AndroidNotificationDetails(
+          channel.id, channel.name,
+          channelDescription: channel.description);
+      var details = NotificationDetails(android: androidNotiDetails);
+
+      if (notification != null) {
+        flutterLocalNotificationsPlugin.show(notification.hashCode,
+            notification.title, notification.body, details);
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((event) {
+      print(event);
+    });
+  }
+
   @override
   void initState() {
+    initFCM();
     super.initState();
+
     _checkPermission();
     _loadData();
   }
